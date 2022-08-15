a9ef6d2dc7866beec3bbda7b0dce6958e33a587b
Bhunaker Vipul <vipulbhunaker1910@gmail.com>
2022-08-15 16:10:00 +0530

    updated css and js logic

--- change.js
+++ change.js
@@ -7,17 +7,16 @@ var availabenotes=[2000,500,200,100,50,20,10,5,2,1]
 checkbutton.addEventListener("click",amountlogic);
 function amountlogic(){
 if(billAmount.value>0){
-  if(cashAmount.value > billAmount.value){
+  if(cashAmount.value >= billAmount.value){
     var diff_amount=cashAmount.value-billAmount.value;
     noofnotes(diff_amount);
-  }
-  else{ 
+  } else{ 
     msg="Cash Amount cannot be less than Bill Amount";
     showmsg(msg);
   }
 }
 else{
-    msg="Bill   Amount should be greater than 0";
+    msg="Bill Amount should be greater than 0";
     showmsg(msg);
 }
 }
